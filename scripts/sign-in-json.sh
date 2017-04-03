7654f15adc130d4529ead0dc4585f97655860cc0
kmg1434 <kmg143@gmail.com>
2017-04-03 11:44:46 -0400

    sign-in notes

--- scripts/sign-in-json.sh
+++ scripts/sign-in-json.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 #curl "http://localhost:3000/sign-in" \
-curl "http://httpbin.org/post" \
-  --include \
-  --request POST \
-  --header "Content-Type: application/json" \
-  --data ""
+# curl "http://httpbin.org/post" \
+#   --include \
+#   --request POST \
+#   --header "Content-Type: application/json" \
+#   --data ""
+// EMAIL=ava@bob.com PASSWORD=hannah scripts/sign-in-with-json.sh
+
+  curl --include --request POST http://localhost:4741/sign-in \
+    --header "Content-Type: application/json" \
+    --data '{
+      "credentials": {
+        "email": " ' "${EMAIL}" ' ",
+        "password": " ' "${PASSWORD}" ' ",
+        "password_confirmation": " ' "${PASSWORD}" ' "
+      }
+    }'
 
 # data output from curl doesn't have a trailing newline
 echo
